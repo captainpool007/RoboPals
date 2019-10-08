4f9f319bbacbf8a5b84743950fdff0cb1d2283b2
captainpool007 <startupdevta@gmail.com>
2019-10-08 21:17:55 +0530

    Updates

---
+++ precache-manifest.4acd4925d68c21e967df1b0a2aec91b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e09242bee8ea46fba1ac11a19653e23",
+    "url": "/RoboPals/index.html"
+  },
+  {
+    "revision": "92e7427b8d27d7f30f2c",
+    "url": "/RoboPals/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "74cf579c7ce07fe9aa19",
+    "url": "/RoboPals/static/css/main.f5ebf016.chunk.css"
+  },
+  {
+    "revision": "92e7427b8d27d7f30f2c",
+    "url": "/RoboPals/static/js/2.677b0c05.chunk.js"
+  },
+  {
+    "revision": "74cf579c7ce07fe9aa19",
+    "url": "/RoboPals/static/js/main.6b66d002.chunk.js"
+  },
+  {
+    "revision": "04b2e30d5d29335bdc32",
+    "url": "/RoboPals/static/js/runtime~main.c808dfb2.js"
+  }
+]);
